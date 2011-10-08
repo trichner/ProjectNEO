44b11d391f6ca28acecc2584d80a475c50435bdc
marcel.mohler <marcel.mohler@8abe7bf7-0363-f839-8fb7-f9cb1719fa0e>
2011-10-08 15:50:16 +0000

    added bluetooth permission added bluetooth "turn-on" request

--- ProjectNEO/trunk/src/ch/baws/projectneo/ProjectNEOActivity.java
+++ ProjectNEO/trunk/src/ch/baws/projectneo/ProjectNEOActivity.java
@@ -1,6 +1,8 @@
 package ch.baws.projectneo;
 
 import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.content.Intent;
 import android.os.Bundle;
 
 public class ProjectNEOActivity extends Activity {
@@ -9,5 +11,10 @@ public class ProjectNEOActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        if (!mBluetoothAdapter.isEnabled()) {
+            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBtIntent, 1);
+        }
     }
 }
