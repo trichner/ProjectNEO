8a8d17e7275794f277cd5ab135a85b3cdff7c2bc
marcel.mohler <marcel.mohler@8abe7bf7-0363-f839-8fb7-f9cb1719fa0e>
2011-10-08 15:50:16 +0000

    added bluetooth permission added bluetooth "turn-on" request

--- gen/ch/baws/projectneo/R.java
+++ gen/ch/baws/projectneo/R.java
@@ -13,6 +13,12 @@ public final class R {
     public static final class drawable {
         public static final int icon=0x7f020000;
     }
+    public static final class id {
+        public static final int button1=0x7f050003;
+        public static final int toggleButton1=0x7f050001;
+        public static final int toggleButton2=0x7f050002;
+        public static final int toggleButton3=0x7f050000;
+    }
     public static final class layout {
         public static final int main=0x7f030000;
     }
