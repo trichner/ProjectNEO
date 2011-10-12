f3bc700350d4299f87a87850216bb2e74c0d5400
marcel.mohler <marcel.mohler@8abe7bf7-0363-f839-8fb7-f9cb1719fa0e>
2011-10-12 08:50:59 +0000

    added menu button (send)

--- src/ch/baws/projectneo/ProjectNEOActivity.java
+++ src/ch/baws/projectneo/ProjectNEOActivity.java
@@ -4,8 +4,14 @@ import android.app.Activity;
 import android.bluetooth.BluetoothAdapter;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class ProjectNEOActivity extends Activity {
+	
+	/** Id des Menüeintrags. */
+	public static final int SEND_ID = Menu.FIRST;
+	  
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -17,4 +23,21 @@ public class ProjectNEOActivity extends Activity {
             startActivityForResult(enableBtIntent, 1);
         }
     }
+    
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+      menu.add(0, SEND_ID, Menu.NONE, "send");
+      return super.onCreateOptionsMenu(menu);
+      
+    }
+    
+    
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//        switch (item.getItemId()) {
+//          case SEND_ID:
+//        	  break;
+//        	  
+//        	  default:
+        		  
+        	  
 }
