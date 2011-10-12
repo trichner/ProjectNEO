a5c26c089f6fd1a53df6f5324c078826317898d0
marcel.mohler <marcel.mohler@8abe7bf7-0363-f839-8fb7-f9cb1719fa0e>
2011-10-12 12:16:49 +0000

    added temp icons, added loc strings

--- gen/ch/baws/projectneo/R.java
+++ gen/ch/baws/projectneo/R.java
@@ -14,15 +14,22 @@ public final class R {
         public static final int icon=0x7f020000;
     }
     public static final class id {
-        public static final int toggleButton1=0x7f050001;
-        public static final int toggleButton2=0x7f050002;
-        public static final int toggleButton3=0x7f050000;
+        public static final int color=0x7f060004;
+        public static final int send=0x7f060003;
+        public static final int toggleButton1=0x7f060001;
+        public static final int toggleButton2=0x7f060002;
+        public static final int toggleButton3=0x7f060000;
     }
     public static final class layout {
         public static final int main=0x7f030000;
     }
+    public static final class menu {
+        public static final int option_menu=0x7f050000;
+    }
     public static final class string {
         public static final int app_name=0x7f040001;
+        public static final int color=0x7f040002;
         public static final int hello=0x7f040000;
+        public static final int send=0x7f040003;
     }
 }
--- src/ch/baws/projectneo/ProjectNEOActivity.java
+++ src/ch/baws/projectneo/ProjectNEOActivity.java
@@ -1,16 +1,17 @@
 package ch.baws.projectneo;
 
+
 import android.app.Activity;
 import android.bluetooth.BluetoothAdapter;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 public class ProjectNEOActivity extends Activity {
-	
-	/** Id des Menüeintrags. */
-	public static final int SEND_ID = Menu.FIRST;
+
 	  
     /** Called when the activity is first created. */
     @Override
@@ -18,6 +19,14 @@ public class ProjectNEOActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
         BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        
+        // If the adapter is null, then Bluetooth is not supported
+        if (mBluetoothAdapter == null) {
+            Toast.makeText(this, "Bluetooth is not available", Toast.LENGTH_LONG).show();
+            finish();
+            return;
+        }
+        
         if (!mBluetoothAdapter.isEnabled()) {
             Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
             startActivityForResult(enableBtIntent, 1);
@@ -26,18 +35,25 @@ public class ProjectNEOActivity extends Activity {
     
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-      menu.add(0, SEND_ID, Menu.NONE, "send");
-      return super.onCreateOptionsMenu(menu);
-      
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.option_menu, menu);
+        return true;
     }
     
     
-//    public boolean onOptionsItemSelected(MenuItem item) {
-//        switch (item.getItemId()) {
-//          case SEND_ID:
-//        	  break;
-//        	  
-//        	  default:
-        		  
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+        case R.id.send:
+            // Launch the DeviceListActivity to see devices and do scan
+            //Intent serverIntent = new Intent(this, DeviceListActivity.class);
+            //startActivityForResult(serverIntent, REQUEST_CONNECT_DEVICE);
+            return true;
+        case R.id.color:
+            // Ensure this device is discoverable by others
+            //ensureDiscoverable();
+            return true;
+        }
+        return false;
+    } 		  
         	  
 }
