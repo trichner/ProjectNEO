ffb27bb1274240c9a0c347cd4f8e7874329745d7
marcel.mohler <marcel.mohler@8abe7bf7-0363-f839-8fb7-f9cb1719fa0e>
2011-10-12 07:54:04 +0000

    button layout changed (menu buttons)

--- ProjectNEO/trunk/gen/ch/baws/projectneo/R.java
+++ ProjectNEO/trunk/gen/ch/baws/projectneo/R.java
@@ -14,7 +14,6 @@ public final class R {
         public static final int icon=0x7f020000;
     }
     public static final class id {
-        public static final int button1=0x7f050003;
         public static final int toggleButton1=0x7f050001;
         public static final int toggleButton2=0x7f050002;
         public static final int toggleButton3=0x7f050000;
