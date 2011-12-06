30319d98c79eb49d2121b347f601de63877d2704
marcel.mohler <marcel.mohler@8abe7bf7-0363-f839-8fb7-f9cb1719fa0e>
2011-12-06 12:22:41 +0000

    Added new icon Added effectsActivity & layout Remade option-menu Connect & Sent merged

---
+++ ProjectNEO/trunk/src/ch/baws/projectneo/EffectActivity.java
@@ -0,0 +1,13 @@
+package ch.baws.projectneo;
+
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class EffectActivity extends Activity {
+	public void onCreate(Bundle bndl)
+	{
+    	super.onCreate(bndl);
+    	setContentView(R.layout.effects);
+	}
+}
