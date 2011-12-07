06313b5079631ef8e3ff6cce330703514e523d64
marcel.mohler <marcel.mohler@8abe7bf7-0363-f839-8fb7-f9cb1719fa0e>
2011-12-07 15:46:57 +0000

    Added method emptyArray tweked emptyArray & randomArray to be able to use dynamic sized arrays

--- src/ch/baws/projectneo/GeneralUtils.java
+++ src/ch/baws/projectneo/GeneralUtils.java
@@ -11,16 +11,36 @@ public class GeneralUtils {
 		return String.valueOf(c);
 		
 	}
-	
-	static int[][] randomArray()
+	/**
+	 * static method randomArray
+	 * creates an array with random entries
+	 * @return
+	 */
+	static int[][] randomArray(int m,int n)
 	{
 		Random rand = new Random();
-		int[][] arr = new int[8][8];
-		for(int i=0;i<8;i++){
-			for(int j=0;j<8;j++){
-				arr[i][j] = rand.nextInt(2);
+		int[][] array = new int[m][n];
+		for(int i=0;i<m;i++){
+			for(int j=0;j<n;j++){
+				array[i][j] = rand.nextInt(2);
 			}
 		}	
-		return arr;
+		return array;
+	}
+	
+	/**
+	 * static method emptyArray
+	 * creates an array with all entries = zero
+	 * @return
+	 */
+	static int[][] emptyArray(int m, int n)
+	{
+	int[][] array = new int[m][n]; // default Array, makes sure it contains zeros
+    	for (int i=0;i<m;i++){
+    		for (int j=0;j<n;j++){
+    			array[i][j] = 0;
+    		}
+    	}
+		return array;
 	}
 }
