b2cb10f979ad34692e859a6afee94a3d69c5e8b9
thomasrichner <thomasrichner@gmail.com>
2011-12-07 21:41:02 +0000

    added snake functionalities

--- src/ch/baws/projectneo/effects/Snake.java
+++ src/ch/baws/projectneo/effects/Snake.java
@@ -4,9 +4,9 @@ import java.util.Random;
 
 public class Snake extends Thread{
 	private class BodyPart{
-		private int x;
-		private int y;
-		public BodyPart nextBody;
+		int x;
+		int y;
+		BodyPart nextBody;
 		public BodyPart(int x, int y){
 			this.x = x;
 			this.y = y;
@@ -16,50 +16,106 @@ public class Snake extends Thread{
 	private static final boolean D = true;
 	private static final String TAG = "SNAKE_THREAD";
 	
+	private boolean EXIT = false;
 	
+	public enum Dir{RIGHT,LEFT,UP,DOWN};
 	
 	private BodyPart head;
 	
 	private Random rand;
 	
+	private Dir dir;
+	
 	public Snake(){
+		create();
+	}
+	
+	private void create(){
 		rand = new Random();
-		head=new BodyPart(rand.nextInt(8), rand.nextInt(8));
+		head=new BodyPart(4, 4);
+		dir = Dir.DOWN;
 		
+		BodyPart temp1,temp2;
+		temp2 = head;
 		for(int i=0;i<3;i++){
-			
+			temp1 = new BodyPart(head.x+1,head.y);
+			temp2.nextBody = temp1;
+			temp2 = temp1;
 		}
-		
+	}
+	
+	
+	public boolean isValidMove(Dir direction){
+		switch(dir){
+		case    UP:
+			if(head.y==7)
+				return false;
+			else
+				return true;
+		case RIGHT:
+			if(head.x==7)
+				return false;
+			else
+				return true;
+		case  DOWN:
+			if(head.y==0)
+				return false;
+			else
+				return true;
+		case  LEFT:
+			if(head.x==0)
+				return false;
+			else
+				return true;
+		}
+		return false;
 	}
 	
 	public void run(){
 		
 		
 		//move and test if move is possible
-		
-		
-		
-		try {
-			this.sleep(500);
-		} catch (InterruptedException e) {
+		BodyPart next = null;
+		while(!EXIT){
+			//Game Over ?
+			if(!isValidMove(dir)){
+				create();
+			}
 			
+			switch(dir){
+			case    UP:
+				next = new BodyPart(head.x,head.y+1);
+				break;
+			case RIGHT:
+				next = new BodyPart(head.x+1,head.y);
+				break;
+			case  DOWN:
+				next = new BodyPart(head.x,head.y-1);
+					break;
+			case  LEFT:
+				next = new BodyPart(head.x-1,head.y);
+				break;
+			}
+	
+			next.nextBody = head;
+			head = next;		
+		
+			//Speed of Snake :)
+			try {
+				this.sleep(500);
+			} catch (InterruptedException e) {	}
 		}
 	}
 	
-	public int[][] array(){
-		
-		
-		
-		
-		return null;
+	public void exit(){
+		EXIT = true;
 	}
 	
 	public void turnLeft(){
 		
 	}
 	public void turnRight(){
-		
-		
+			
 	}
 
 }
