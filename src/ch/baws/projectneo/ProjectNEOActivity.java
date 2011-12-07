f3dd29f1cac40d6ea405d25956584abc01670efa
marcel.mohler <marcel.mohler@8abe7bf7-0363-f839-8fb7-f9cb1719fa0e>
2011-12-07 16:23:40 +0000

    Button digits now start at 0

--- src/ch/baws/projectneo/ProjectNEOActivity.java
+++ src/ch/baws/projectneo/ProjectNEOActivity.java
@@ -24,22 +24,28 @@ public class ProjectNEOActivity extends Activity {
 
 	
 	private static final String TAG = "PN_ACTIVITY";
+
+	public static final String SEND_KEY = "snd";
 	private static final boolean D = true;
 	public int[][] colorArray; // array to store the current LED colors
+
 	public SendTimer snd; // Timertask
+	public Timer timer;
+	
 	public boolean connected = false;
 
 	private OutputStream outStream = null;
 
 	private BluetoothUtils Bluetooth = null;
-	Button button11, button12, button13, button14, button15, button16, button17, button18; // all buttons
-	Button button21, button22, button23, button24, button25, button26, button27, button28;
-	Button button31, button32, button33, button34, button35, button36, button37, button38;
-	Button button41, button42, button43, button44, button45, button46, button47, button48;
-	Button button51, button52, button53, button54, button55, button56, button57, button58;
-	Button button61, button62, button63, button64, button65, button66, button67, button68;
-	Button button71, button72, button73, button74, button75, button76, button77, button78;
-	Button button81, button82, button83, button84, button85, button86, button87, button88;
+	
+	Button button00, button01, button02, button03, button04, button05, button06, button07;
+	Button button10, button11, button12, button13, button14, button15, button16, button17; // all buttons
+	Button button20, button21, button22, button23, button24, button25, button26, button27;
+	Button button30, button31, button32, button33, button34, button35, button36, button37;
+	Button button40, button41, button42, button43, button44, button45, button46, button47;
+	Button button50, button51, button52, button53, button54, button55, button56, button57;
+	Button button60, button61, button62, button63, button64, button65, button66, button67;
+	Button button70, button71, button72, button73, button74, button75, button76, button77;
 	
     /** Called when the activity is first created. */
     @Override
@@ -47,23 +53,27 @@ public class ProjectNEOActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
         
-       colorArray = new int[9][9]; // default Array, makes sure it contains zeros
-        for (int i=0;i<9;i++){
-        	for (int j=0;j<9;j++){
-        		colorArray[i][j] = 0;
-        	}
-        }	
+        colorArray = GeneralUtils.emptyArray(8,8); // fills array with zeros
         
-        
-        button11 = (Button) findViewById(R.id.button11); // makes sure we can use buttonxx as a variable
+        button00 = (Button) findViewById(R.id.button00);
+        button01 = (Button) findViewById(R.id.button01); // makes sure we can use buttonxx like a variable
+        button02 = (Button) findViewById(R.id.button02);
+        button03 = (Button) findViewById(R.id.button03);
+        button04 = (Button) findViewById(R.id.button04);
+        button05 = (Button) findViewById(R.id.button05);
+        button06 = (Button) findViewById(R.id.button06);
+        button07 = (Button) findViewById(R.id.button07);
+ 
+        button10 = (Button) findViewById(R.id.button10);
+        button11 = (Button) findViewById(R.id.button11); 
         button12 = (Button) findViewById(R.id.button12);
         button13 = (Button) findViewById(R.id.button13);
         button14 = (Button) findViewById(R.id.button14);
         button15 = (Button) findViewById(R.id.button15);
         button16 = (Button) findViewById(R.id.button16);
         button17 = (Button) findViewById(R.id.button17);
-        button18 = (Button) findViewById(R.id.button18);
         
+        button20 = (Button) findViewById(R.id.button20);     
         button21 = (Button) findViewById(R.id.button21);
         button22 = (Button) findViewById(R.id.button22);
         button23 = (Button) findViewById(R.id.button23);
@@ -71,8 +81,8 @@ public class ProjectNEOActivity extends Activity {
         button25 = (Button) findViewById(R.id.button25);
         button26 = (Button) findViewById(R.id.button26);
         button27 = (Button) findViewById(R.id.button27);
-        button28 = (Button) findViewById(R.id.button28);
         
+        button30 = (Button) findViewById(R.id.button30);
         button31 = (Button) findViewById(R.id.button31);
         button32 = (Button) findViewById(R.id.button32);
         button33 = (Button) findViewById(R.id.button33);
@@ -80,8 +90,8 @@ public class ProjectNEOActivity extends Activity {
         button35 = (Button) findViewById(R.id.button35);
         button36 = (Button) findViewById(R.id.button36);
         button37 = (Button) findViewById(R.id.button37);
-        button38 = (Button) findViewById(R.id.button38);
         
+        button40 = (Button) findViewById(R.id.button40);
         button41 = (Button) findViewById(R.id.button41);
         button42 = (Button) findViewById(R.id.button42);
         button43 = (Button) findViewById(R.id.button43);
@@ -89,8 +99,8 @@ public class ProjectNEOActivity extends Activity {
         button45 = (Button) findViewById(R.id.button45);
         button46 = (Button) findViewById(R.id.button46);
         button47 = (Button) findViewById(R.id.button47);
-        button48 = (Button) findViewById(R.id.button48);
         
+        button50 = (Button) findViewById(R.id.button50);
         button51 = (Button) findViewById(R.id.button51);
         button52 = (Button) findViewById(R.id.button52);
         button53 = (Button) findViewById(R.id.button53);
@@ -98,8 +108,8 @@ public class ProjectNEOActivity extends Activity {
         button55 = (Button) findViewById(R.id.button55);
         button56 = (Button) findViewById(R.id.button56);
         button57 = (Button) findViewById(R.id.button57);
-        button58 = (Button) findViewById(R.id.button58);
         
+        button60 = (Button) findViewById(R.id.button60);
         button61 = (Button) findViewById(R.id.button61);
         button62 = (Button) findViewById(R.id.button62);
         button63 = (Button) findViewById(R.id.button63);
@@ -107,8 +117,8 @@ public class ProjectNEOActivity extends Activity {
         button65 = (Button) findViewById(R.id.button65);
         button66 = (Button) findViewById(R.id.button66);
         button67 = (Button) findViewById(R.id.button67);
-        button68 = (Button) findViewById(R.id.button68);
         
+        button70 = (Button) findViewById(R.id.button70);
         button71 = (Button) findViewById(R.id.button71);
         button72 = (Button) findViewById(R.id.button72);
         button73 = (Button) findViewById(R.id.button73);
@@ -116,16 +126,6 @@ public class ProjectNEOActivity extends Activity {
         button75 = (Button) findViewById(R.id.button75);
         button76 = (Button) findViewById(R.id.button76);
         button77 = (Button) findViewById(R.id.button77);
-        button78 = (Button) findViewById(R.id.button78);
-        
-        button81 = (Button) findViewById(R.id.button81);
-        button82 = (Button) findViewById(R.id.button82);
-        button83 = (Button) findViewById(R.id.button83);
-        button84 = (Button) findViewById(R.id.button84);
-        button85 = (Button) findViewById(R.id.button85);
-        button86 = (Button) findViewById(R.id.button86);
-        button87 = (Button) findViewById(R.id.button87);
-        button88 = (Button) findViewById(R.id.button88);
         
         
         Bluetooth = new BluetoothUtils();
@@ -228,7 +228,7 @@ public class ProjectNEOActivity extends Activity {
         	Toast.makeText(getApplicationContext(), "Connected", Toast.LENGTH_SHORT).show();
         	connected = true; //TODO
       		
-       		Timer timer = new Timer(); // daemon to send current colorcode
+       		timer = new Timer(); // daemon to send current colorcode
        		snd = new SendTimer();
        		snd.setBluetooth(Bluetooth);
        		snd.setArray(colorArray);
@@ -241,10 +241,10 @@ public class ProjectNEOActivity extends Activity {
 
         
         case R.id.effects:
-        	Toast.makeText(getApplicationContext(), "Nothing to see here", Toast.LENGTH_SHORT).show();
-//            final Intent intent = new Intent(this,
-//                    EffectActivity.class);
-//        	startActivity(intent);
+        	//Toast.makeText(getApplicationContext(), "Nothing to see here", Toast.LENGTH_SHORT).show();
+            final Intent intent = new Intent(this,EffectActivity.class);           
+        	startActivity(intent);
+        	timer.cancel();
         	return true;
         
         }
@@ -256,7 +256,26 @@ public class ProjectNEOActivity extends Activity {
  */
 public void toggleColor(View v){
 	
-	if (button11.getId() == ((Button)v).getId())
+	if (button00.getId() == ((Button)v).getId())
+		toggle(button00, colorArray, 0, 0);
+	else if (button01.getId() == ((Button)v).getId())
+		toggle(button01, colorArray, 0, 1);
+	else if (button02.getId() == ((Button)v).getId())
+		toggle(button02, colorArray, 0, 2);
+	else if (button03.getId() == ((Button)v).getId())
+		toggle(button03, colorArray, 0, 3);
+	else if (button04.getId() == ((Button)v).getId())
+		toggle(button04, colorArray, 0, 4);
+	else if (button05.getId() == ((Button)v).getId())
+		toggle(button05, colorArray, 0, 5);
+	else if (button06.getId() == ((Button)v).getId())
+		toggle(button06, colorArray, 0, 6);
+	else if (button07.getId() == ((Button)v).getId())
+		toggle(button07, colorArray, 0, 7);
+
+	if (button10.getId() == ((Button)v).getId())
+		toggle(button10, colorArray, 1, 0);
+	else if (button11.getId() == ((Button)v).getId())
 		toggle(button11, colorArray, 1, 1);
 	else if (button12.getId() == ((Button)v).getId())
 		toggle(button12, colorArray, 1, 2);
@@ -270,10 +289,10 @@ public void toggleColor(View v){
 		toggle(button16, colorArray, 1, 6);
 	else if (button17.getId() == ((Button)v).getId())
 		toggle(button17, colorArray, 1, 7);
-	else if (button18.getId() == ((Button)v).getId())
-		toggle(button18, colorArray, 1, 8);
 	
-	if (button21.getId() == ((Button)v).getId())
+	if (button20.getId() == ((Button)v).getId())
+		toggle(button20, colorArray, 2, 0);
+	else if (button21.getId() == ((Button)v).getId())
 		toggle(button21, colorArray, 2, 1);
 	else if (button22.getId() == ((Button)v).getId())
 		toggle(button22, colorArray, 2, 2);
@@ -287,10 +306,10 @@ public void toggleColor(View v){
 		toggle(button26, colorArray, 2, 6);
 	else if (button27.getId() == ((Button)v).getId())
 		toggle(button27, colorArray, 2, 7);
-	else if (button28.getId() == ((Button)v).getId())
-		toggle(button28, colorArray, 2, 8);
 
-	if (button31.getId() == ((Button)v).getId())
+	if (button30.getId() == ((Button)v).getId())
+		toggle(button30, colorArray, 3, 0);
+	else if (button31.getId() == ((Button)v).getId())
 		toggle(button31, colorArray, 3, 1);
 	else if (button32.getId() == ((Button)v).getId())
 		toggle(button32, colorArray, 3, 2);
@@ -304,10 +323,10 @@ public void toggleColor(View v){
 		toggle(button36, colorArray, 3, 6);
 	else if (button37.getId() == ((Button)v).getId())
 		toggle(button37, colorArray, 3, 7);
-	else if (button38.getId() == ((Button)v).getId())
-		toggle(button38, colorArray, 3, 8);
-	
-	if (button41.getId() == ((Button)v).getId())
+
+	if (button40.getId() == ((Button)v).getId())
+		toggle(button40, colorArray, 4, 0);
+	else if (button41.getId() == ((Button)v).getId())
 		toggle(button41, colorArray, 4, 1);
 	else if (button42.getId() == ((Button)v).getId())
 		toggle(button42, colorArray, 4, 2);
@@ -321,9 +340,9 @@ public void toggleColor(View v){
 		toggle(button46, colorArray, 4, 6);
 	else if (button47.getId() == ((Button)v).getId())
 		toggle(button47, colorArray, 4, 7);
-	else if (button48.getId() == ((Button)v).getId())
-		toggle(button48, colorArray, 4, 8);
-	
+
+	else if (button50.getId() == ((Button)v).getId())
+		toggle(button50, colorArray, 5, 0);
 	if (button51.getId() == ((Button)v).getId())
 		toggle(button51, colorArray, 5, 1);
 	else if (button52.getId() == ((Button)v).getId())
@@ -338,10 +357,10 @@ public void toggleColor(View v){
 		toggle(button56, colorArray, 5, 6);
 	else if (button57.getId() == ((Button)v).getId())
 		toggle(button57, colorArray, 5, 7);
-	else if (button58.getId() == ((Button)v).getId())
-		toggle(button58, colorArray, 5, 8);
-	
-	if (button61.getId() == ((Button)v).getId())
+
+	if (button60.getId() == ((Button)v).getId())
+		toggle(button60, colorArray, 6, 0);	
+	else if (button61.getId() == ((Button)v).getId())
 		toggle(button61, colorArray, 6, 1);
 	else if (button62.getId() == ((Button)v).getId())
 		toggle(button62, colorArray, 6, 2);
@@ -355,10 +374,10 @@ public void toggleColor(View v){
 		toggle(button66, colorArray, 6, 6);
 	else if (button67.getId() == ((Button)v).getId())
 		toggle(button67, colorArray, 6, 7);
-	else if (button68.getId() == ((Button)v).getId())
-		toggle(button68, colorArray, 6, 8);
-	
-	if (button71.getId() == ((Button)v).getId())
+
+	if (button70.getId() == ((Button)v).getId())
+		toggle(button70, colorArray, 7, 0);
+	else if (button71.getId() == ((Button)v).getId())
 		toggle(button71, colorArray, 7, 1);
 	else if (button72.getId() == ((Button)v).getId())
 		toggle(button72, colorArray, 7, 2);
@@ -372,25 +391,7 @@ public void toggleColor(View v){
 		toggle(button76, colorArray, 7, 6);
 	else if (button77.getId() == ((Button)v).getId())
 		toggle(button77, colorArray, 7, 7);
-	else if (button78.getId() == ((Button)v).getId())
-		toggle(button78, colorArray, 7, 8);
-	
-	if (button81.getId() == ((Button)v).getId())
-		toggle(button81, colorArray, 8, 1);
-	else if (button82.getId() == ((Button)v).getId())
-		toggle(button82, colorArray, 8, 2);
-	else if (button83.getId() == ((Button)v).getId())
-		toggle(button83, colorArray, 8, 3);
-	else if (button84.getId() == ((Button)v).getId())
-		toggle(button84, colorArray, 8, 4);
-	else if (button85.getId() == ((Button)v).getId())
-		toggle(button85, colorArray, 8, 5);
-	else if (button86.getId() == ((Button)v).getId())
-		toggle(button86, colorArray, 8, 6);
-	else if (button87.getId() == ((Button)v).getId())
-		toggle(button87, colorArray, 8, 7);
-	else if (button88.getId() == ((Button)v).getId())
-		toggle(button88, colorArray, 8, 8);
+
 }
 
 /**
