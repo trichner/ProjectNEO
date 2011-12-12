e0fd3d2fa9bbd2528392b6b824441903075e9d46
thomasrichner@gmail.com <thomasrichner@gmail.com@8abe7bf7-0363-f839-8fb7-f9cb1719fa0e>
2011-12-12 15:05:53 +0000

    emulatormode

--- ProjectNEO/trunk/src/ch/baws/projectneo/SendTimer.java
+++ ProjectNEO/trunk/src/ch/baws/projectneo/SendTimer.java
@@ -40,17 +40,26 @@ public class SendTimer extends TimerTask{
 		
 		if (D)	Log.e(TAG, "try to send array, size: " + arr.length + "*" + arr[0].length);
 		if(arr!=null && arr.length==8 && arr[0].length==8){
-			if (D)	Log.e(TAG, "sending the array");
-			try{
-				//effectdrawer.draw(arr); //prints the array
-				this.bluetooth.send(arr);
-			} catch (RuntimeException e) {
-				Log.e(TAG, "ON SEND: Runtime Exception, size: "+ arr.length + "*" + arr[0].length, e);				
+			
+			if(bluetooth!=null){
+				if (D)	Log.e(TAG, "sending the array");
+				try{
+					
+					this.bluetooth.send(arr);
+				} catch (RuntimeException e) {
+					Log.e(TAG, "ON SEND: Runtime Exception, size: "+ arr.length + "*" + arr[0].length, e);				
+				}
+			}else{
+				if (D)	Log.e(TAG, "not sending, in emulator mode");
 			}
+			
+			//effectdrawer.draw(arr); //prints the array
+			
 		}
 		else{
 			if (D)	Log.e(TAG, "array is malformed");
 		}
+		
 	}
 	
 	
