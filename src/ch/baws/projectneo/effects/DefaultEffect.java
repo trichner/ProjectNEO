379c2b230674734bcbb4caa28a7236ce4af36b43
thomasrichner <thomasrichner@gmail.com>
2011-12-28 19:36:54 +0000

    added service

---
+++ src/ch/baws/projectneo/effects/DefaultEffect.java
@@ -0,0 +1,18 @@
+package ch.baws.projectneo.effects;
+
+import ch.baws.projectneo.GeneralUtils;
+
+public class DefaultEffect extends Effect{
+
+	@Override
+	public int[][] getArray() {
+		return GeneralUtils.emptyArray(8, 8);
+	}
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
--- src/ch/baws/projectneo/sendService/SendService.java
+++ src/ch/baws/projectneo/sendService/SendService.java
@@ -1,8 +1,10 @@
-package sendService;
+package ch.baws.projectneo.sendService;
 
 import java.util.concurrent.ScheduledThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
 import ch.baws.projectneo.ProjectMORPHEUS;
+import ch.baws.projectneo.effects.Effect;
 
 import android.app.Service;
 import android.content.Intent;
@@ -18,9 +20,10 @@ public class SendService extends Service {
 	private ScheduledThreadPoolExecutor executor;
 	private SendTimer sendTimer;
 	
+	private ProjectMORPHEUS application = (ProjectMORPHEUS) getApplication();
+	
 	@Override
 	public IBinder onBind(Intent arg0) {
-		// TODO Auto-generated method stub
 		return null;
 	}
 	
@@ -28,7 +31,23 @@ public class SendService extends Service {
 	public void onCreate(){
 		if (D)		Log.e(TAG, "initialise SendJob");
 		executor = new ScheduledThreadPoolExecutor(1);
-		sendTimer = new SendTimer((ProjectMORPHEUS) getApplication());
+		
+		sendTimer = new SendTimer(application);
+	}
+
+	@Override
+	public void onDestroy() {
+		super.onDestroy();
+		executor.shutdown();
+		application.getEffect().exit();
+	}
+
+	@Override
+	public void onStart(Intent intent, int startId) {
+		super.onStart(intent, startId);
+		Effect effect = application.getEffect();
+		if(!effect.isAlive()) effect.start();
+		executor.scheduleAtFixedRate(sendTimer, 50, 1/FPS, TimeUnit.MILLISECONDS);
 	}
 	
 	
