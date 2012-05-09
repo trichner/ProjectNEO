9b3e34d0a78438f3d5d768c6406d8c88aa393b21
thomasrichner <thomasrichner@gmail.com>
2012-05-09 22:02:25 +0000

    added Snake Activity

---
+++ src/ch/baws/projectneo/effects/SnakePlayer.java
@@ -0,0 +1,37 @@
+package ch.baws.projectneo.effects;
+
+import ch.baws.projectneo.GeneralUtils;
+import ch.baws.projectneo.R;
+import ch.baws.projectneo.SnakeActivity;
+
+public class SnakePlayer extends Effect {
+
+	
+	public SnakePlayer() {
+		super("Philipp Boesch","Snake!");
+		this.icon = R.drawable.ic_app; //TODO cool icon!
+		this.activity = SnakeActivity.class;
+	}
+	
+	@Override
+	public int[][] getArray() {
+		//TODO
+		return GeneralUtils.getEmpty8x8();
+	}
+
+	@Override
+	public void run() {
+		
+		while(!EXIT){
+			
+			
+			try {
+				sleep(1000);
+			} catch (InterruptedException e) {
+
+			}
+		}
+		
+	}
+
+}
