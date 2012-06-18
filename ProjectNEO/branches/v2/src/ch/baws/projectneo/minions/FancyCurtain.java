50fba66b69f155492cb85341fbd0ff0b1adb9a11
thomasrichner@gmail.com <thomasrichner@gmail.com@8abe7bf7-0363-f839-8fb7-f9cb1719fa0e>
2012-06-18 15:43:01 +0000

    added a new human snake player

---
+++ ProjectNEO/branches/v2/src/ch/baws/projectneo/minions/FancyCurtain.java
@@ -0,0 +1,36 @@
+package ch.baws.projectneo.minions;
+
+import ch.baws.projectneo.GeneralUtils;
+import ch.baws.projectneo.effects.Effect;
+import ch.baws.projectneo.frameGenerator.Bitfields;
+
+public class FancyCurtain extends Effect{
+	private int[][] array;
+	private int color;
+	private int speed;
+	private long bf;
+	
+	public FancyCurtain(int color,int duration){
+		array = GeneralUtils.getEmpty8x8();
+		speed = duration/9;
+		bf=0;
+	}
+	@Override
+	public void run(){
+		for(int i=0;i<9 && !EXIT;i++){
+			array = Bitfields.toNEOArr(bf, 0, color, 0);
+			try {
+				sleep(speed);
+			} catch (InterruptedException e) {}
+			bf = (bf >>> 8) | Bitfields.BORDER_U; //we could also exploit the way signed shift works...
+		}
+	}
+	@Override
+	public int[][] getArray() {
+		return array;
+	}
+	
+	public long getBitMap(){
+		return bf;
+	}
+}	
